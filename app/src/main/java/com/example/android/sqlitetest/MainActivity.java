ce75df5bc4700b07f821704031bc1c3e77cde83e
linucksrox <ericdalyblog@dalydays.com>
2017-05-22 07:26:11 -0400

    Start setting up MainActivity, but still need CursorAdapter

--- app/src/main/java/com/example/android/sqlitetest/MainActivity.java
+++ app/src/main/java/com/example/android/sqlitetest/MainActivity.java
@@ -13,7 +13,7 @@ import com.example.android.sqlitetest.data.ProductHelper;
 
 public class MainActivity extends AppCompatActivity {
 
-    ProductHelper mDbHelper = new ProductHelper(this);
+    ProductHelper mDbHelper;
     TextView mProductList;
     Button mAddProductButton;
     Button mDeleteProductButton;
@@ -30,43 +30,43 @@ public class MainActivity extends AppCompatActivity {
         mAddProductButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                addProduct();
+//                addProduct();
                 printDatabase();
             }
         });
 
-        mDeleteProductButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                deleteProduct();
-                printDatabase();
-            }
-        });
+//        mDeleteProductButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                deleteProduct();
+//                printDatabase();
+//            }
+//        });
 
         printDatabase();
     }
 
-    public long addProduct() {
-        // get the database in write mode
-        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+//    public long addProduct() {
+//        // get the database in write mode
+//        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+//
+//        // create map of values
+//        ContentValues values = new ContentValues();
+//        values.put(ProductEntry.COLUMN_NAME_NAME, "sample product");
+//
+//        // insert the new row, returning the primary key value of the new row
+//        long newRowId = db.insert(ProductEntry.TABLE_NAME, null, values);
+//
+//        return newRowId;
+//    }
 
-        // create map of values
-        ContentValues values = new ContentValues();
-        values.put(ProductEntry.COLUMN_NAME_NAME, "sample product");
-
-        // insert the new row, returning the primary key value of the new row
-        long newRowId = db.insert(ProductEntry.TABLE_NAME, null, values);
-
-        return newRowId;
-    }
-
-    public void deleteProduct() {
-        SQLiteDatabase db = mDbHelper.getWritableDatabase();
-
-        String selection = ProductEntry.COLUMN_NAME_NAME + " LIKE ?";
-        String[] selectionArgs = { "sample product" };
-        db.delete(ProductEntry.TABLE_NAME, selection, selectionArgs);
-    }
+//    public void deleteProduct() {
+//        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+//
+//        String selection = ProductEntry.COLUMN_NAME_NAME + " LIKE ?";
+//        String[] selectionArgs = { "sample product" };
+//        db.delete(ProductEntry.TABLE_NAME, selection, selectionArgs);
+//    }
 
     public void printDatabase() {
         // clear current list
@@ -87,13 +87,5 @@ public class MainActivity extends AppCompatActivity {
             mProductList.append(cursor.getString(cursor.getColumnIndexOrThrow(ProductEntry.COLUMN_NAME_NAME)) + "\n");
         }
         cursor.close();
-    }
-
-    @Override
-    protected void onDestroy() {
-        // close that connection!
-        mDbHelper.close();
-
-        super.onDestroy();
     }
 }
